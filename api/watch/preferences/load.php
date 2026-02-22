f165801db08b9c9f29e2b20756cd923ec431c3bc
Vilcinsh <mrkaqkis@gmail.com>
2026-02-22 19:49:42 +0200

    Watch section update

--- api/watch/preferences/load.php
+++ api/watch/preferences/load.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../../config/config.php';
+require __DIR__ . '/../../../config/config.php';
 require BASE_PATH . '/core/Guard.php';
 
 header('Content-Type: application/json');
